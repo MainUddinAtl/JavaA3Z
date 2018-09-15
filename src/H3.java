94efa2fafabb7d608ca986b498f493aaac1b52f9
Main Uddin <main06@gmail.com>
2018-09-15 00:26:42 -0400

    file modified

--- src/H3.java
+++ src/H3.java
@@ -3,7 +3,7 @@ public class H3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("THis is another Hellow world message");
+		System.out.println("THis is another Hellow world messages");
 		System.out.println("Adding new changes or new line");
 	}
 
